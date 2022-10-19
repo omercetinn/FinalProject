f51627325e19962c80c60db4d66040e07f1dedcb
ömer <mer@DESKTOP-6S1VIE5>
2022-10-19 16:26:30 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,67 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product> {
+            new Product{ProductId=1,CategoryId=1,ProductName="Bardak",UnitPrice=15,UnitsInStock=15},
+            new Product{ProductId=2,CategoryId=1,ProductName="Kamera",UnitPrice=1500,UnitsInStock=12},
+            new Product{ProductId=3,CategoryId=2,ProductName="Telefon",UnitPrice=15000,UnitsInStock=1},
+            new Product{ProductId=4,CategoryId=2,ProductName="Fare",UnitPrice=15,UnitsInStock=10},
+            new Product{ProductId=5,CategoryId=2,ProductName="Klavye",UnitPrice=15,UnitsInStock=13}
+            
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            //LINQ - LANGUAFE INTEGRATED QUERY
+            //Product productToDelete = null;
+            //foreach (var p in _products)
+            //{
+            //    if (product.ProductId==p.ProductId)
+            //    {
+            //        productToDelete = p;
+            //    } 
+            //}
+
+            Product productToDelete = _products.SingleOrDefault(p=>p.ProductId == product.ProductId);
+
+            _products.Remove(productToDelete);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public void Update(Product product)
+        {
+            //gönderdiğim ürün idsine sahip ürünü bul
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.UnitPrice = product.UnitPrice;  
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+            productToUpdate.CategoryId = product.CategoryId;
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+    }
+}
