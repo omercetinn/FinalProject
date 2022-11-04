4af8b9b1476fc67f9f5b883d6e587a8d01e7c6ad
ömer <mer@DESKTOP-6S1VIE5>
2022-11-04 10:19:58 +0300

    new project

---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //context : Db tabloları ile proje classlarını bağlamak
+    public class NorthwindContext:DbContext
+    {
+    }
+}
